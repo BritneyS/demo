b0f910733ea473df0238bb71ae3b9af5f6eb3241
Britney Smith <brit.smith7@gmail.com>
2020-11-30 16:54:10 -0500

    Expanded Book model

--- src/main/kotlin/Book.kt
+++ src/main/kotlin/Book.kt
@@ -1,2 +1,9 @@
-class Book {
-}
+import org.springframework.data.annotation.Id
+import java.util.UUID
+
+data class Book(
+    @Id
+    val id: String = UUID.randomUUID().toString(),
+    val author: String,
+    val title: String
+)
