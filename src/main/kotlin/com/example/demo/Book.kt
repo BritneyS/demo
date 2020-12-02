5ad0a5b674f7adf834d3cb1e8ca41772da8a24f5
Britney Smith <brit.smith7@gmail.com>
2020-12-02 09:49:48 -0500

    add Book model to common package + add appropriate mongodb related annotations

--- src/main/kotlin/com/example/demo/Book.kt
+++ src/main/kotlin/com/example/demo/Book.kt
@@ -1,6 +1,12 @@
+package com.example.demo
+
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
 import java.util.UUID
 
+@Document("books")
+@TypeAlias("books")
 data class Book(
     @Id
     val id: String = UUID.randomUUID().toString(),
