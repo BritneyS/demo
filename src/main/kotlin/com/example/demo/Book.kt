95608dd82af2432ff14941737cebfbd18f686f1d
Britney Smith <brit.smith7@gmail.com>
2020-12-03 10:42:11 -0500

    Added createdOn and updatedOn timestamps to Book model

--- src/main/kotlin/com/example/demo/Book.kt
+++ src/main/kotlin/com/example/demo/Book.kt
@@ -1,8 +1,11 @@
 package com.example.demo
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
 import java.util.UUID
 
 @Document("books")
@@ -11,5 +14,9 @@ data class Book(
     @Id
     val id: String = UUID.randomUUID().toString(),
     val author: String,
-    val title: String
+    val title: String,
+    @CreatedDate
+    val createdOn: Instant = Instant.now(),
+    @LastModifiedDate
+    val updatedOn: Instant
 )
