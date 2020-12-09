9ae2a1466b8c01fb35537422a06323991adbe622
Britney Smith <brit.smith7@gmail.com>
2020-12-09 10:45:27 -0500

    enable auditing (createddate/lastmodifieddate); lastmodifieddate bug wip

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,8 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 
 @SpringBootApplication
+@EnableMongoAuditing
 class DemoApplication
 
 fun main(args: Array<String>) {
